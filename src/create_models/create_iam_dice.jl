bfa4e7aa30076cd32a4538f153467640bdd99e35
Frank Errickson <frankerrickson@berkeley.edu>
2019-11-05 12:04:31 -0800

    Add files to create modifed versions of DICE and FUND
    Modify so DICE and FUND use results from various versions of SNEASY

---
+++ src/create_models/create_iam_dice.jl
@@ -0,0 +1,33 @@
+#----------------------------------------------------------------------------------------
+# This function creates an instance of DICE used to calculate the SC-CH4 with SNEASY+CH4.
+#----------------------------------------------------------------------------------------
+
+# Load required packages.
+using Mimi
+using MimiDICE2013
+
+
+function create_iam_dice(;end_year::Int=2300)
+
+    # Create an instance of DICE2013.
+    m = MimiDICE2013.get_model()
+
+    # Set model time horizon.
+    set_dimension!(m, :time, 2010:5:end_year)
+
+    # Calculate default number of model timesteps.
+    n_steps = length(dim_keys(m, :time))
+
+    # Remove climate and welfare components.
+    delete!(m, :emissions)
+    delete!(m, :co2cycle)
+    delete!(m, :radiativeforcing)
+    delete!(m, :climatedynamics)
+    delete!(m, :welfare)
+
+    # Set a placeholder for temperature anomaly in DICE2013 damage function.
+    set_param!(m, :damages, :TATM, zeros(n_steps))
+
+    # Return modified version of DICE2013.
+    return m
+end
