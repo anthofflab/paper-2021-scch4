a836afc8a6a177248637204ec5adff86dab16d76
Frank <frankerrickson@berkeley.edu>
2020-03-27 16:45:26 -0700

    Edit error check for DICE
    Some extreme parameter combinations can either produce model errors or non-plausible marginal damages in DICE (e.g. annual damages > $1,000,000 from one tonne of CH4).

--- src/scch4/dice_scch4.jl
+++ src/scch4/dice_scch4.jl
@@ -81,9 +81,9 @@ function dice_damages(base_temp::Array{Float64,2}, pulse_temp::Array{Float64,2},
         end
     end
 
-    # Distinguish between DICE model indices that caused a model error and those that did not.
-    error_indices = findall(x-> x == -99999.99, annual_marginal_damages[:,1])
-    good_indices  = findall(x-> x != -99999.99, annual_marginal_damages[:,1])
+    # Distinguish between DICE model indices that caused a model error or yield non-plausible outcomes and those that did not.
+    error_indices = findall(x-> x <= -99999.99 || x > 1_000_000, annual_marginal_damages[:,end])
+    good_indices  = findall(!in(error_indices), collect(1:number_samples))
 
     # Return results.
     return annual_marginal_damages, pc_consumption_base, error_indices, good_indices
